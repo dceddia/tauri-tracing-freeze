bd39dd2f4a9016d3478840f0d8eb183bd68d5559
Dave Ceddia <dave@daveceddia.com>
2024-04-12 09:47:17 -0400

    Added 'tracing' and lots of events

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::time::Duration;
+use tauri::Manager;
 
 fn main() {
-    tauri::Builder::default()
+    let app = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .invoke_handler(tauri::generate_handler![])
+        .build(tauri::generate_context!())
+        .unwrap();
+
+    app.run(|app, event| {
+        match event {
+            tauri::RunEvent::Ready => {
+                // start sending events
+                let app_clone = app.clone();
+                std::thread::spawn(move || loop {
+                    std::thread::sleep(Duration::from_millis(10));
+                    app_clone.emit("hello", ()).unwrap();
+                });
+            }
+            _ => {}
+        }
+    });
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,18 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import { event } from "@tauri-apps/api";
+
+  let count = 0;
+  event.listen("hello", () => {
+    count++;
+  });
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
+  <h1>tracing freeze repro</h1>
 
   <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
+    Events received: {count}
   </p>
 
-  <div class="row">
-    <Greet />
-  </div>
-
-
+  <p>Try resizing the window and see if it freezes.</p>
 </main>
-
-<style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
-</style>
